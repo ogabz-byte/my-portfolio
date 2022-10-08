5c503533ea2ac1182d6aee1d1b2bb1324d98e999
ogabido somto <112892790+ogabz-byte@users.noreply.github.com>
2022-10-08 20:10:57 +0100

    Update Data.jsx

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -5,21 +5,21 @@ import Web from "../assets/projects/web.png";
 export const data = [
   {
     id: 1,
-    name: "React JS Application",
+    name: "Minishop",
     image: Shop,
     github: "https://github.com/ogabz-byte/mini-ecommerce",
     live: "https://ogabzminishop.netlify.app",
   },
   {
     id: 2,
-    name: "React JS Application",
+    name: "Weather Forecast",
     image: Weather,
     github: "https://github.com/ogabz-byte/main-weather-app",
     live: "https://ogabz-weather-app.netlify.app",
   },
   {
     id: 3,
-    name: "React JS Application",
+    name: "Web",
     image: Web,
     github: "https://github.com/ogabz-byte/bootstrap5",
     live: "https://ogabzbootstrap.netlify.app",
