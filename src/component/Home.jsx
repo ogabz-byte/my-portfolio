34c03ad14bdeca790536ff6751432553abb802f5
ogabido somto <112892790+ogabz-byte@users.noreply.github.com>
2023-01-11 04:36:31 -0800

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -10,13 +10,13 @@ const Home = () => {
         <h1 className="text-3xl sm:text-5xl font-bold text-[#ccd6f6]">
           OGABIDO SOMTOCHUKWU
         </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Full Stack Developer.
+        <h2 className="text-3xl sm:text-6xl font-bold text-[#8892b0]">
+          I'm a Full Stack Developer and Designer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           I’m a full-stack developer specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive full-stack web applications.
+          building responsive full-stack web applications and mobile apps.
         </p>
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
