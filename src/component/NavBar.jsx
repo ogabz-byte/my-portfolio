01ce8e35f3285dc527bb294fa34496ae7ce2e7e3
ogabido somto <112892790+ogabz-byte@users.noreply.github.com>
2023-01-11 04:32:28 -0800

    Update NavBar.jsx

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -110,7 +110,7 @@ const Navbar = () => {
               className="flex justify-between items-center w-full text-gray-300"
               href="https://dribbble.com/ogabz"
             >
-              Github <FaDribbbleSquare size={30} />
+              Dribble <FaDribbbleSquare size={30} />
             </a>
           </li>
  <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
