5204b7981efb9dfefee9ca448ab0de7bf6e08f4b
ogabido somto <112892790+ogabz-byte@users.noreply.github.com>
2026-04-13 11:25:43 +0100

    Update email address in Contact component

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
             Contact
           </p>
           <p className="text-gray-300 py-4">
-            Submit the form below or shoot me an email - ogabidosomto@gmail.com
+            Submit the form below or shoot me an email - ogabsomto@gmail.com
           </p>
         </div>
         <input
