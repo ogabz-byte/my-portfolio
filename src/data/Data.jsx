c99ef0eedbeaedd9fb422d623f45b47a9d2c384f
ogabido somto <112892790+ogabz-byte@users.noreply.github.com>
2026-04-13 11:21:09 +0100

    Update project data and live links in Data.jsx
    Rearranged project entries and updated DakatPay live link.

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -4,25 +4,26 @@ import Dakat from "../assets/projects/dakat.png";
 
 
 export const data = [
+  
   {
     id: 1,
+    name: "DakatPay: A MVP Fintech app simplifying secure digital transactions for individuals and businesses.",
+    image: Dakat,
+    github: "",
+    live: "https://main.d29c42zb09u2hu.amplifyapp.com/dakatpay",
+  },
+  {
+    id: 2,
     name: "An online store with advanced shopping features",
     image: Shop,
     github: "https://github.com/ogabz-byte/amazon",
     live: "https://amazona-eight.vercel.app/",
   },
-  {
-    id: 2,
-    name: "DakatPay: A MVP Fintech app simplifying secure digital transactions for individuals and businesses.",
-    image: Dakat,
-    github: "",
-    live: "https://dakay-pay-website.vercel.app/",
-  },
-  {
-    id: 3,
-    name: "A Headset shopping store with payment integration",
-    image: Head,
-    github: "https://github.com/ogabz-byte/ecommerce",
-    live: "https://ecommerce-seven-rosy.vercel.app/",
-  },
+  // {
+  //   id: 3,
+  //   name: "A Headset shopping store with payment integration",
+  //   image: Head,
+  //   github: "https://github.com/ogabz-byte/ecommerce",
+  //   live: "https://ecommerce-seven-rosy.vercel.app/",
+  // },
 ];
