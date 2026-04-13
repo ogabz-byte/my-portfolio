c6f13120e30a92a73da9c5a594b033eafc35c9b1
ogabido somto <112892790+ogabz-byte@users.noreply.github.com>
2026-04-13 11:24:13 +0100

    Change font size class for item name display

--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -28,7 +28,7 @@ const Work = () => {
             >
               {/* Hover effect for images */}
               <div className="opacity-0 group-hover:opacity-100 ">
-                <span className="text-2xl font bold text-white tracking-wider ">
+                <span className="text-xl font bold text-white tracking-wider ">
                   {item.name}
                 </span>
                 <div className="pt-8 text-center ">
